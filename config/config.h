2b082e55d644fe6497ee707164f66d9b84d439f7
Quanghaictu <dtc225200400@ictu.edu.vn>
2026-06-30 09:44:06 +0700

    fix ip

--- config/config.h
+++ config/config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define BROKER_IP    "192.168.1.140"   
+#define BROKER_IP    "192.168.1.189"   
 #define BROKER_PORT  1883
 #define CLIENT_ID    "imx6ull_led1"
 #define TOPIC        "test/topic"
